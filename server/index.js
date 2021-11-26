ab49b174e9407ed9da3e3311b0a48e92371859a5
Jin SungJun <wlstjdwns115@gmail.com>
2021-11-26 15:16:56 +0900

    ADD: .eslintrc , .prettierrc

---
+++ server/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const https = require('https');
+const cookieParser = require('cookie-parser');
+const { sequelize } = require('./models');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+const HTTPS_PORT = process.env.HTTPS_PORT || 4000;
+
+sequelize.sync();
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
+  }),
+);
+
+app.use(cookieParser());
+
+let server;
+console.log(`server running at ${HTTPS_PORT}`);
+server = app.listen(HTTPS_PORT);
+
+module.exports = server;
