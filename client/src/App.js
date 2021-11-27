13f6a35097084364d674d702beb439705c8caf23
Jin SungJun <wlstjdwns115@gmail.com>
2021-11-27 11:36:57 +0900

    UPDATE: client 부분 App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
       })
       .catch(err => console.log(err));
   }, []);
-  return <div className="App"></div>;
+  return <div className="App">테스트용으로 보이는지 알아보기</div>;
 }
 
 export default App;
