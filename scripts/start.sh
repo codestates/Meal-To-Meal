6801ed12857b6fafa377c888e4d61f2ca31463fe
Jin SungJun <wlstjdwns115@gmail.com>
2021-11-27 15:58:39 +0900

    FIX: start.sh내 오타수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,4 +6,4 @@ export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --name
 export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PORT --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
 
-authbind --deep pm2 start app.js
+authbind --deep pm2 start index.js
