251fb51362cf200753368210b5201c7eefd35622
Hendrix1995 <hgud55@naver.com>
2021-11-27 18:20:39 +0900

    FIX: 클라이언트 components Footer 위치 조절

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,8 +7,8 @@ function Footer() {
       <div className="footer-container">
         <div className="footer-our-info-container">
           <img className="logo" src={require('../img/meal-to-meal-logo-192.png').default} alt="" />
-          <div className="footer-title-text">EteamMerge</div>
-          <div className="footer-text">문의사항: </div>
+          <div className="footer-title-text">Sudo_Hired</div>
+          <div className="footer-text">문의사항:</div>
           <div className="footer-text">hgud55@naver.com</div>
         </div>
         <div className="footer-our-info-container">
@@ -25,7 +25,7 @@ function Footer() {
           <div className="footer-text">Hendrix Lim</div>
         </div>
       </div>
-      <div className="footer-copyright">Copyright 2021 sudo_hired, All rights reserved.</div>
+      <div className="footer-copyright">Copyright 2021 Sudo_Hired, All rights reserved.</div>
     </>
   );
 }
