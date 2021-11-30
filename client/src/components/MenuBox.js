d5d9ffd2c18154a8ec33efcbd433c330d25a809d
Hendrix1995 <hgud55@naver.com>
2021-12-01 02:50:10 +0900

    FIX: 클라이언트 Page StoreInfo, components MenuBox, ReviewBox 디자인

--- client/src/components/MenuBox.js
+++ client/src/components/MenuBox.js
@@ -3,36 +3,19 @@ import React from 'react';
 function MenuBox() {
   return (
     <div className="menuBox-container">
-      <div className="menuBox-title">메뉴</div>
-      <div className="menuBox-menu-container">
-        <img className="storeinfo-food-image" src={require('../img/dummy/원조할매국밥.png').default} alt="" />
-        <div className="menuBox-info-container">
-          <div className="menuBox-food-name">원조 할매 국밥</div>
-          <div className="menuBox-food-price">6,800원</div>
-          <div className="menuBox-donation-container">
-            <img className="storeinfo-icon" src={require('../img/donation.png').default} alt="" />
-            <div className="menuBox-food-remain">현재 기부한 그릇 수 : 3</div>
-          </div>
-          <div className="menuBox-button-container">
-            <button className="menuBox-Donatebutton">기부하기</button>
-            <button className="menuBox-Eatbutton">먹기</button>
+      <div className="menu-container">
+        <img className="menu-food-image" src={require('../img/dummy/원조할매국밥.png').default} alt="" />
+        <div className="menu-info-container">
+          <span className="menu-text">원조 할매 국밥</span>
+          <span className="menu-text">6,800원</span>
+          <div className="menu-donation-container">
+            <img className="menu-donation-icon" src={require('../img/donation.png').default} alt="" />
+            <span className="menu-text">기부받은 그릇 : 3</span>
           </div>
         </div>
-      </div>
-      <div className="menuBox-menu-container">
-        <img className="storeinfo-food-image" src={require('../img/dummy/원조할매국밥.png').default} alt="" />
-
-        <div className="menuBox-info-container">
-          <div className="menuBox-food-name">원조 할매 국밥</div>
-          <div className="menuBox-food-price">6,800원</div>
-          <div className="menuBox-donation-container">
-            <img className="storeinfo-icon" src={require('../img/donation.png').default} alt="" />
-            <div className="menuBox-food-remain">현재 기부한 그릇 수 : 3</div>
-          </div>
-          <div className="menuBox-button-container">
-            <button className="menuBox-Donatebutton">기부하기</button>
-            <button className="menuBox-Eatbutton">먹기</button>
-          </div>
+        <div className="menu-button-container">
+          <button className="menu-donate-button">기부하기</button>
+          <button className="menu-eat-button">먹기</button>
         </div>
       </div>
     </div>
--- client/src/pages/StoreInfo.js
+++ client/src/pages/StoreInfo.js
@@ -11,47 +11,40 @@ function StoreInfo({ isLogin, setIsLogin }) {
 
   return (
     <>
-      <Header isLogin={isLogin} setIsLogin={setIsLogin} />
-      <div className="storeinfo-pagecontainer">
-        <div className="storeinfo-box">
-          <div className="storeinfo-info-container">
-            <div className="storeinfo-title">가게 정보</div>
+      <Header />
+      <div className="storeinfo-container">
+        <div className="storeinfo-title">가게 정보</div>
+        <div className="storeinfo-title-container">
+          <div className="store-title-container">
+            <img className="store-category-icon" src={require('../img/찌개.png').default} alt="" />
+            <div className="store-title">원조 할매 국밥</div>
           </div>
-          <div className="storeinfo-info-container">
-            <img className="storeinfo-categoryimage" src={require('../img/찌개.png').default} alt="" />
-            <div className="storeinfo-storename">원조 할매 국밥</div>
-            <div className="storeinfo-category">한식</div>
+          <div className="store-category-text">한식</div>
+        </div>
+        <img className="store-img" src={require('../img/dummy/store1.png').default} alt="" />
+        <div className="store-info-container">
+          <div className="store-detail-info-container">
+            <i className="fas fa-map-marker-alt" />
+            <div className="store-detail-info-text">서울특별시 용산구 이태원동 119-23</div>
           </div>
-          <div className="storeinfo-info-container">
-            <img className="storeinfo-storeimage" src={require('../img/dummy/store1.png').default} alt="" />
+          <div className="store-detail-info-container">
+            <i className="fas fa-clock" />
+            <div className="store-detail-info-text">10:00 ~ 23:00</div>
           </div>
-          <div className="storeinfo-info-container">
-            <img className="storeinfo-icon" src={require('../img/location.png').default} alt="" />
-            <div className="storeinfo-address">서울특별시 용산구 이태원동 119-23</div>
-            <img className="storeinfo-icon" src={require('../img/businesshour.png').default} alt="" />
-            <div className="storeinfo-businesshour">10:00 ~ 23:00</div>
-          </div>
-          <div className="storeinfo-info-container">
-            <img className="storeinfo-icon" src={require('../img/desciption.png').default} alt="" />
-            <div className="storeinfo-description">서울 100년 전통의 국밥! (할머니 안 들어감)</div>
+          <div className="store-detail-info-container">
+            <i className="fas fa-utensils" />
+            <div className="store-detail-info-text">
+              서울 100년 전통의 국밥! (할머니 안 들어감) 근데 들어갈지도 모르는데 정현님 힘내요 왜이렇게 조용해요 자는거
+              아니죠? 제발 정신 차려요! 나 너무 심심행
+            </div>
           </div>
         </div>
-
+        <div className="box-title">메뉴</div>
         <MenuBox />
+        <div className="box-title">리뷰</div>
         <ReviewBox />
-        <div className="storeinfo-button-container">
-          <button className="storeinfo-cartbutton">장바구니</button>
-          <button
-            className="storeinfo-backbutton"
-            onClick={() => {
-              navigate('/map');
-            }}
-          >
-            뒤로 가기
-          </button>
-        </div>
-        <Footer />
       </div>
+      <Footer />
     </>
   );
 }
