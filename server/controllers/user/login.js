450b0d12330e0dddc9a6b4ec1bc5f756a97c6f33
anniemon <seojung19@gmail.com>
2021-12-01 21:12:38 +0900

    UPDATE: tokenAuth 미들웨어 next 빼고 구현 user 회원탈퇴 컨트롤러 구현 user 로그아웃 컨트롤러 구현 user 로그인 컨트롤러 구현 eslintignore에 client index.html 추가

--- server/controllers/user/login.js
+++ server/controllers/user/login.js
@@ -7,9 +7,26 @@ const {
   sendRefreshToken,
 } = require('../../middlewares/tokenFunctions');
 
-module.exports = (req, res) => {
-  // const { user_nickname, user_password } = req.body;
-  // bcrypt.compare(user_password, hash, (err, result) => {
-  //   //result boolean
-  // });
+module.exports = async (req, res) => {
+  const { user_email, user_password } = req.body;
+
+  try {
+    const userInfo = await user.findOne({ where: { user_email: user_email } });
+    const hash = userInfo.user_password;
+
+    await bcrypt.compare(user_password, hash, (err, result) => {
+      if (result) {
+        delete userInfo.dataValues.password;
+        const accessToken = generateAccessToken(userInfo.dataValues);
+        const refreshToken = generateRefreshToken(userInfo.dataValues);
+        sendRefreshToken(res, refreshToken);
+        sendAccessToken(res, accessToken);
+        res.status(200);
+      } else {
+        return res.status(400).json({ message: '아이디, 패스워드를 확인해주세요' });
+      }
+    });
+  } catch (err) {
+    return res.status(400).json({ message: '아이디, 패스워드를 확인해주세요' });
+  }
 };
