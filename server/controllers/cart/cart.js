d83035fee74c90050a0e3498523ea4b98840e907
anniemon <seojung19@gmail.com>
2021-12-06 00:10:10 +0900

    ADD: cart 모델, 시드, 컨트롤러 초안 패스워드, 닉네임 수정 구현 store-user fk 추가 menu-store fk 추가 cart-user fk 추가 cart_menu 테이블 생성 cart_menu 테이블에 cart_id, menu_id fk 추가 user seed 4개 더 추가 (비번 수정 필요) store seed에 user_id 추가 cart_menu seed 생성 accessToken expiresIn 테스트용으로 200s로 변경

--- server/controllers/cart/cart.js
+++ server/controllers/cart/cart.js
@@ -1,3 +1,20 @@
 module.exports = {
-  post: data => {},
+  post: async (req, res) => {
+    //이런 식으로 보내준다.
+    const cart = [
+      {
+        menu_id: 5,
+        order_quantity: 3,
+      },
+      {
+        menu_id: 3,
+        order_quantity: 2,
+      },
+    ];
+    const orderList = req.body.cart;
+    const total_price = req.body.total_price;
+    for (let order of orderList) {
+    }
+  },
+  get: async (req, res) => {},
 };
