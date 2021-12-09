3f2673e4d3505bcbc27a53d6235619ce6cb133c0
Hendrix1995 <hgud55@naver.com>
2021-12-10 02:04:33 +0900

    FIX: Sidebar open 중 외부영역 클릭시 close

--- client/src/components/Sidebar/SidebarNotLogin.js
+++ client/src/components/Sidebar/SidebarNotLogin.js
@@ -11,17 +11,19 @@ function SidebarNotLogin({ openLoginModalHandler, openisNotLoginOpenSidebarHandl
     openisNotLoginOpenSidebarHandler();
   };
   return (
-    <div className="sidebar-container">
-      <div className="sidebar-menu-container" onClick={() => clickLogin()}>
-        <div className="sidebar-icon">&#x1F373;</div>
-        <div className="sidebar-text">로그인</div>
-      </div>
-      <div className="sidebar-menu-container" onClick={() => clickSignup()}>
-        <div className="sidebar-icon">&#x1F9C0;</div>
-        <div className="sidebar-text">회원가입</div>
-      </div>
-      <div className="sidebar-menu-container">
-        <img className="sidebar-logo" src={require('../../img/meal-to-meal-logo-192.png').default} alt="" />
+    <div className="sidebar-backdrop" onClick={() => openisNotLoginOpenSidebarHandler()}>
+      <div className="sidebar-container" onClick={e => e.stopPropagation()}>
+        <div className="sidebar-menu-container" onClick={() => clickLogin()}>
+          <div className="sidebar-icon">&#x1F373;</div>
+          <div className="sidebar-text">로그인</div>
+        </div>
+        <div className="sidebar-menu-container" onClick={() => clickSignup()}>
+          <div className="sidebar-icon">&#x1F9C0;</div>
+          <div className="sidebar-text">회원가입</div>
+        </div>
+        <div className="sidebar-menu-container">
+          <img className="sidebar-logo" src={require('../../img/meal-to-meal-logo-192.png').default} alt="" />
+        </div>
       </div>
     </div>
   );
