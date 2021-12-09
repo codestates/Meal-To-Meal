27e5015a0054cd48e4110ff2a4216a86944cc269
Hendrix1995 <hgud55@naver.com>
2021-12-09 20:43:47 +0900

    ADD: SearchResultSidebar

--- client/src/components/Map/Search.js
+++ client/src/components/Map/Search.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function Search_input() {
+function Search({ isOpenSearchResultSidebar, setIsOpenSearchResultSidebar, setSearchResult }) {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchTextChange = e => {
+    setSearchText(e.target.value);
+  };
+
+  const searchHandler = () => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/search?keyword=${searchText}`, {
+        headers: { 'Content-Type': 'application/json' },
+      })
+      .then(res => {
+        setSearchResult(res.data.searchResult);
+        setIsOpenSearchResultSidebar(!isOpenSearchResultSidebar);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="search-container">
-      <input className="search-store-input" placeholder="가게를 검색하세요." />
-      <i className="fas fa-search" />
+      <input className="search-store-input" placeholder="가게를 검색하세요." onChange={handleSearchTextChange} />
+      <i className="fas fa-search" onClick={() => searchHandler()} />
     </div>
   );
 }
 
-export default Search_input;
+export default Search;
