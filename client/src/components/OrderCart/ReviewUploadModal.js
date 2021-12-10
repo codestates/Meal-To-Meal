93110da9bac0d0bfa3efd46ee35e09f43a287391
Hendrix1995 <hgud55@naver.com>
2021-12-10 16:21:22 +0900

    FIX: 이미지 업로드 서버 연결 성공

--- client/src/components/OrderCart/ReviewUploadModal.js
+++ client/src/components/OrderCart/ReviewUploadModal.js
@@ -1,47 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import AWS from 'aws-sdk';
 import axios from 'axios';
+import Loading from '../Loading';
 
 function ReviewUploadModal({ navigate, openReviewModalHandler, orderedMeal, setOrderedMeal }) {
   const accessToken = localStorage.getItem('accessToken');
-  const [reviewInfo, setReviewInfo] = useState({
-    reviewImage: '',
-    reviewText: '',
-  });
+  const [isLoading, setIsLoading] = useState(true);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [reviewText, setReviewText] = useState(null);
 
-  const handleChange = key => e => {
-    setReviewInfo({ ...reviewInfo, [key]: e.target.value });
-  };
-  const reviewSubmitHandler = e => {
-    const { reviewImage, reviewText } = reviewInfo;
-    const imageFile = e.reviewImage.slice(12);
+  // 이미지 업로드
+  const fileInput = useRef();
 
-    axios
-      .post(
-        `${process.env.REACT_APP_API_URL}/review`,
-        {
-          store_id: orderedMeal[0].menu.store.store_id,
-          menu_id: orderedMeal[0].menu.menu_id,
-          review_image: reviewImage,
-          review_content: reviewText,
-        },
-        { headers: { authorization: `Bearer ${accessToken}` }, withCredentials: true }
-      )
-      .then(res => {
-        alert('리뷰가 등록되었습니다');
-        navigate('/maps');
-        console.log('------------스토어', orderedMeal[0].menu.store.store_id);
-        console.log('------------메뉴', orderedMeal[0].menu.menu_id);
-        setOrderedMeal([]);
-      })
-      .catch(err => {
-        console.log(err);
-        alert('리뷰 등록 에러남!');
-      });
+  const handleClick = e => {
+    e.preventDefault();
+    const s3 = new AWS.S3();
+    (async () => {
+      await s3
+        .putObject({
+          Body: fileInput.current.files[0],
+          Bucket: 'meal2sdk',
+          Key: fileInput.current.files[0].name,
+        })
+        .promise();
+    })();
+
+    // 악시오스 조져!!
   };
+
+  // const reviewSubmitHandler = e => {
+  //   console.log(selectedFile);
+  //   console.log(reviewText);
+  //   axios
+  //     .post(
+  //       `${process.env.REACT_APP_API_URL}/review`,
+  //       {
+  //         store_id: orderedMeal[0].menu.store.store_id,
+  //         menu_id: orderedMeal[0].menu.menu_id,
+  //         review_image: selectedFile,
+  //         review_content: reviewText,
+  //       },
+  //       { headers: { authorization: `Bearer ${accessToken}` }, withCredentials: true }
+  //     )
+  //     .then(res => {
+  //       alert('리뷰가 등록되었습니다');
+  //       navigate('/maps');
+  //       setOrderedMeal([]);
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //       alert('리뷰 등록 에러남!');
+  //     });
+  // };
+
   return (
     <div className="review-upload-container">
       <div className="review-upload-backdrop">
-        <div className="review-upload-window">
+        <form className="review-upload-window" onSubmit={handleClick}>
           <div className="review-upload-title-container">
             <div className="review-upload-title">음식점 리뷰</div>
             <i className="fa fa-times" onClick={openReviewModalHandler} />
@@ -52,25 +67,16 @@ function ReviewUploadModal({ navigate, openReviewModalHandler, orderedMeal, setO
           </div>
           <div className="review-upload-store-name">{orderedMeal[0].menu.menu_name}</div>
           <div className="review-upload-content-container">
-            <img className="review-upload-food-image" src={reviewInfo.reviewImage} alt="" />
-            <textarea
-              className="review-upload-food-text"
-              placeholder="리뷰를 적어주세요"
-              onChange={handleChange('reviewText')}
-            />
+            <img className="review-upload-food-image" src={selectedFile} alt="" />
+            <textarea className="review-upload-food-text" placeholder="리뷰를 적어주세요" onChange={setSelectedFile} />
           </div>
           <div className="review-upload-button-container">
-            <input
-              className="review-upload-image-upload-button"
-              type="file"
-              accept="image/x-png, image/gif, image/jpeg"
-              onChange={handleChange('reviewImage')}
-            />
-            <button className="review-upload-submit-button" onClick={() => reviewSubmitHandler(reviewInfo)}>
+            <input className="review-upload-image-upload-button" type="file" accept="image/*" ref={fileInput} />
+            <button className="review-upload-submit-button" type="submit">
               등록하기
             </button>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
