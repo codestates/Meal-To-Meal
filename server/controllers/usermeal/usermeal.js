c31da94a084ac6d1ef7880e11cac5b8c2ed203c3
anniemon <seojung19@gmail.com>
2021-12-12 00:49:53 +0900

    FIX: user-meal 조회 시 내역 없을 때 404 뜨는 거 삭제

--- server/controllers/usermeal/usermeal.js
+++ server/controllers/usermeal/usermeal.js
@@ -64,13 +64,9 @@ module.exports = {
           ],
           where: { user_id: userInfo.id },
         });
-        if (!userMeal) {
-          res.status(404).json({ message: '주문 내역이 없습니다' });
-        } else {
-          const matchedUser = await user.findOne({ where: { id: userMeal.user_id } });
-          delete matchedUser.dataValues.user_password;
-          res.status(200).json({ userMeal, matchedUser });
-        }
+        const matchedUser = await user.findOne({ where: { id: userMeal.user_id } });
+        delete matchedUser.dataValues.user_password;
+        res.status(200).json({ userMeal, matchedUser });
       } catch (err) {
         res.status(400).json({ message: err.message });
       }
