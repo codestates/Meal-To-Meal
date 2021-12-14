66c436580eba8457bb6fda44ea29da00e1b558b9
Hendrix1995 <hgud55@naver.com>
2021-12-14 20:38:07 +0900

    FIX: 지도 검색 이후 결과를 누르면 center이동

--- client/src/pages/Maps.js
+++ client/src/pages/Maps.js
@@ -22,6 +22,9 @@ const Map = () => {
   const [icon, setIcon] = useState('');
   const [storeList, setStoreList] = useState([]);
   const [selected, setSelected] = useState(null);
+  const [isOpenSearchResultSidebar, setIsOpenSearchResultSidebar] = useState(false);
+  const [searchResult, setSearchResult] = useState([]);
+  const [isChangeCenter, setIsChangeCenter] = useState({ lat: 37.51249519205713, lng: 126.99480974427608, zoom: 13 });
 
   const getImage = e => {
     if (e === '분식') return 분식;
@@ -54,9 +57,16 @@ const Map = () => {
 
   return (
     <GoogleMap
-      defaultZoom={13}
       defaultCenter={{ lat: 37.51249519205713, lng: 126.99480974427608 }}
-      options={{ disableDefaultUI: true }}
+      defaultZoom={13}
+      options={{ disableDefaultUI: true, minZoom: 9, maxZoom: 18 }}
+      zoom={isChangeCenter.zoom}
+      onZoomChanged={() => {
+        setIsChangeCenter({ lat: Number(isChangeCenter.lat), lng: Number(isChangeCenter.lng), zoom: 13 });
+      }}
+      ref={map => {
+        map && map.panTo({ lat: Number(isChangeCenter.lat), lng: Number(isChangeCenter.lng) });
+      }}
     >
       {storeList.map(el => (
         <Marker
@@ -82,38 +92,32 @@ const Map = () => {
           )}
         </Marker>
       ))}
+      <Search
+        isOpenSearchResultSidebar={isOpenSearchResultSidebar}
+        setIsOpenSearchResultSidebar={setIsOpenSearchResultSidebar}
+        setSearchResult={setSearchResult}
+      />
+      {isOpenSearchResultSidebar ? (
+        <SearchResultSidebar searchResult={searchResult} setIsChangeCenter={setIsChangeCenter} />
+      ) : null}
     </GoogleMap>
   );
 };
 
 const WrappedMap = withScriptjs(withGoogleMap(Map));
 
-const GoogleMaps = () => {
-  return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <WrappedMap
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&
-        libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
-        loadingElement={<div style={{ height: '100%' }} />}
-        containerElement={<div style={{ height: '100%' }} />}
-        mapElement={<div style={{ height: '100%' }} />}
-      />
-    </div>
-  );
-};
 const Maps = () => {
-  const [isOpenSearchResultSidebar, setIsOpenSearchResultSidebar] = useState(false);
-  const [searchResult, setSearchResult] = useState([]);
-
   return (
     <div className="map-container">
-      <GoogleMaps />
-      <Search
-        isOpenSearchResultSidebar={isOpenSearchResultSidebar}
-        setIsOpenSearchResultSidebar={setIsOpenSearchResultSidebar}
-        setSearchResult={setSearchResult}
-      />
-      {isOpenSearchResultSidebar ? <SearchResultSidebar searchResult={searchResult} /> : null}
+      <div style={{ width: '100vw', height: '100vh' }}>
+        <WrappedMap
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&
+        libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
+          loadingElement={<div style={{ height: '100%' }} />}
+          containerElement={<div style={{ height: '100%' }} />}
+          mapElement={<div style={{ height: '100%' }} />}
+        />
+      </div>
     </div>
   );
 };
