2837099afcac2c17c13c1d3c49f8878adf81ec64
Hendrix1995 <hgud55@naver.com>
2021-12-15 17:01:09 +0900

    FIX: 렌딩 페이지 버튼 CSS 추가 , 버튼 클릭시 navigate

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -27,7 +27,9 @@ function Landing() {
           <div className="landing-catchphrase-sub">
             돈 없는 돼지보다는 그래도 배부른 돼지가 되자! 맨트를 길게 하면 위치를 맞춰 줄 것입니다!
           </div>
-          <button className="landing-start-button">지금 시작하기!</button>
+          <button className="landing-start-button" onClick={() => navigate('/maps')}>
+            지금 시작하기!
+          </button>
         </div>
         <div className="landing-ani" ref={landingAniContainer} />
       </div>
@@ -36,16 +38,22 @@ function Landing() {
           <div className="landing-tutorials-info-title">먹기를 눌러요!</div>
           <div className="landing-tutorials-info-text">개같이 눌러 보아요!! 와다다다다!!!</div>
         </div>
-        <div className="landing-tutorials-info-gif">gif가 들어갈 공간</div>
+        <video className="landing-tutorials-info-gif" autoPlay loop muted>
+          <source src={landing1} type="video/mp4" />
+        </video>
       </div>
       <div className="landing-tutorials-container-left">
         <div className="landing-tutorial-info-text-container-left">
           <div className="landing-tutorials-info-title-left">먹기를 눌러요!</div>
           <div className="landing-tutorials-info-text-left">개같이 눌러 보아요!! 와다다다다!!!</div>
         </div>
-        <div className="landing-tutorials-info-gif-left">gif가 들어갈 공간</div>
+        <video className="landing-tutorials-info-gif-left" autoPlay loop muted>
+          <source src={landing1} type="video/mp4" />
+        </video>
       </div>
-      <button className="landing-start-button-bottom">지금 시작하기!</button>
+      <button className="landing-start-button-bottom" onClick={() => navigate('/maps')}>
+        지금 시작하기!
+      </button>
     </div>
   );
 }
